0c1b3df4d7b298f37da5f6c610565546c48d737c
Jordan Ulmer <julmer526@gmail.com>
2022-05-19 19:34:53 +0100

    adding Todo into main is working

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,11 +1,11 @@
 // importing the arrays and the DOM element
 import {projectArray,todoArray } from "./todoObjects";
-const main = document.querySelector(".main")
-console.log("main",main)
+
 
 // function to create and append the main section with each todo
 function updateMain () {
     todoArray.forEach(element => {
+        const main = document.querySelector(".main")
         const todo = document.createElement("div")
         todo.classList.toggle("todo")
         main.appendChild(todo)

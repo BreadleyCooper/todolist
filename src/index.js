9ec90e8e300080fe301ab2eb13541c2b2ac7dd22
Jordan Ulmer <julmer526@gmail.com>
2022-05-19 18:20:36 +0100

    addded todo factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log("test")
+import "./style.css"
+import Todo from "./todoObjects"
+// grabbing the root node element
+
+const content = document.querySelector("#content")
+
+// grid container
+const container = document.createElement("div")
+container.classList.toggle("container")
+
+// header
+const header = document.createElement("div")
+header.classList.toggle("header")
+header.textContent = "To Do"
+
+// left sidebar
+const leftSidebar = document.createElement("div")
+leftSidebar.classList.toggle("leftSidebar")
+const viewAll = document.createElement("button");
+viewAll.classList.toggle("viewAll")
+viewAll.textContent = "View All Todos"
+const projects = document.createElement("div")
+projects.classList.toggle("projects")
+projects.textContent = "Projects"
+const addNewProject = document.createElement("button")
+addNewProject.classList.toggle("addNewProject")
+addNewProject.textContent = "+ Add New Project"
+projects.appendChild(addNewProject)
+
+
+// main content
+const main = document.createElement("div")
+main.classList.toggle("main")
+main.textContent = "Main Content"
+
+// footer
+const footer = document.createElement("div")
+footer.classList.toggle("footer")
+footer.textContent = "Created by Jordan Ulmer, 2022"
+// appending to the parent elements
+content.appendChild(container)
+container.append(header, leftSidebar, main, footer)
+leftSidebar.append(viewAll, projects)
+
+const gardening = new Todo("Gardening","Gardening","Urgent","monday")
+gardening.pushProject()
+gardening.pushTodo()
+
+
---
+++ src/todoObjects.js
@@ -0,0 +1,18 @@
+let projectArray = []//push the project name of when a new todo is made in here
+let todoArray = [] //push each todo object into this array to list them easier
+function Todo (name, project, priority, dueDate) {
+    this.name = name;
+    this.project = project;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.pushTodo = function() {
+        todoArray.push(this)
+        console.log(todoArray)
+    }
+    this.pushProject = function() {
+        projectArray.push(this.project)
+        console.log(projectArray)
+    }
+}
+
+export default Todo 
